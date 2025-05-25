6f26e7feef9b746e293ad0aeb93bf82410ccf4fa
Youcef <mekki.d.youcef@outlook.fr>
2025-05-24 21:34:43 -0400

    Added code for console and DB

--- LABO1/lab/src/App.java
+++ LABO1/lab/src/App.java
@@ -1,5 +1,31 @@
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World !");
+    public static void main(String[] args) {
+        try {
+           String databaseUrl = "jdbc:sqlite:LABO1/Magasin.db";
+           ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
+
+            TableUtils.createTableIfNotExists(connectionSource, Produit.class);
+            ProduitDao dao = new ProduitDao(connectionSource);
+
+        
+            System.out.println("Lancement de la caisse...");
+            Thread caisse1 = new Thread(new CaisseService(dao));
+            caisse1.start();
+
+            // Pour simuler plusieurs caisses, tu peux lancer plus de threads ici :
+            // Thread caisse2 = new Thread(new CaisseService(dao));
+            // Thread caisse3 = new Thread(new CaisseService(dao));
+            // caisse2.start(); caisse3.start();
+
+            caisse1.join();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
